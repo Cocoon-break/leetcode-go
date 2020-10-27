75fc191def3a2859b38bd81625f423678c29fa20
wushengping <wushengping@megvii.com>
2020-10-27 11:01:59 +0800

    [add]: getIntersectionNode

---
+++ hot100/linked_list/leetcode_160_test.go
@@ -0,0 +1,15 @@
+package linked_list
+
+import "testing"
+
+//
+func TestGetIntersectionNode(t *testing.T) {
+	l1 := GenListNode([]int{4, 1})
+	l2 := GenListNode([]int{})
+	//公共部分
+	// l3 := GenListNode([]int{8, 4, 5})
+	// l1.Next = l3
+	// l2.Next = l3
+	out := getIntersectionNode(l1, l2)
+	t.Log(out)
+}
